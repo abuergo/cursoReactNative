9bcb277b62c612347f7d35f675df1c43c6a00800
abuergo <alejandrobuergo@gmail.com>
2021-12-07 07:39:40 -0300

    Desafio realizado - Encuentro 1

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hola, Coder!</Text>
+
       <StatusBar style="auto" />
     </View>
   );
